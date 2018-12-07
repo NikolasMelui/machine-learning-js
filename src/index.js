0e6eb90eb689f3cdca0c029d3fc3457b6df7f79f
nikolasmelui <slak@samaradom.ru>
2018-12-07 11:21:39 +0400

    Add signature for the neural network training

--- src/index.js
+++ src/index.js
@@ -1,16 +1,21 @@
-import http from 'http';
+// import http from 'http';
+import brain from 'brain.js';
 import {} from 'dotenv/config';
-import { getServerHost, getServerPort } from './helpers';
+// import { getServerHost, getServerPort } from './helpers';
+const network = new brain.NeuralNetwork();
+network.train();
+const result = network.run();
+console.log(result);
 
-http
-  .createServer((req, res) => {
-    res.writeHead(200, 'OK', { 'Content-Type': 'text/plain' });
-    res.end(
-      `Hello from ${getServerHost(req)}:${getServerPort(
-        req
-      )} and welcome to the multikey-node-boilerplate!`
-    );
-  })
-  .listen(process.env.SERVER_PORT, () =>
-    console.log(`Server is listening on port ${process.env.SERVER_PORT}`)
-  );
+// http
+//   .createServer((req, res) => {
+//     res.writeHead(200, 'OK', { 'Content-Type': 'text/plain' });
+//     res.end(
+//       `Hello from ${getServerHost(req)}:${getServerPort(
+//         req
+//       )} and welcome to the multikey-node-boilerplate!`
+//     );
+//   })
+//   .listen(process.env.SERVER_PORT, () =>
+//     console.log(`Server is listening on port ${process.env.SERVER_PORT}`)
+//   );
